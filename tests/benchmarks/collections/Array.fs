8c2e4f29395896905b51f0ee1c1c2f7035d58beb
Vlad Zarytovskii <vzaritovsky@hotmail.com>
2023-11-08 19:38:15 +0100

    Adjust benchmarks

--- tests/benchmarks/collections/Array.fs
+++ tests/benchmarks/collections/Array.fs
@@ -17,7 +17,7 @@ module ArrayBenchmark =
 [<GenericTypeArguments(typeof<double>)>]
 type ArrayMaxBenchmark<'T when 'T : comparison>() =
 
-    [<Params(1_000, 10_000, 100_000, 1_000_000, 2_000_000)>]
+    [<Params(1, 10, 100, 1_000, 10_000, 100_000, 1_000_000, 2_000_000)>]
     member val ArraySize = 0 with get, set
 
     member val Array : 'T array = Unchecked.defaultof<_> with get, set
@@ -26,13 +26,14 @@ type ArrayMaxBenchmark<'T when 'T : comparison>() =
     member this.Setup() =
         this.Array <- ArrayBenchmark.generateArray<'T> this.ArraySize
 
+    [<Benchmark(Description="Array.max - FSharp.Core.Faster")>]
+    member this.MaxFaster() =
+        FSharp.Core.Faster.Collections.Array.Array.max this.Array
+
     [<Benchmark(Description="Array.max - FSharp.Core")>]
     member this.MaxDefault() =
          Microsoft.FSharp.Collections.Array.max this.Array
 
-    [<Benchmark(Description="Array.max - FSharp.Core.Faster")>]
-    member this.MaxFaster() =
-        FSharp.Core.Faster.Collections.Array.Array.max this.Array
 
 [<GenericTypeArguments(typeof<int>)>]
 [<GenericTypeArguments(typeof<int64>)>]
@@ -47,10 +48,10 @@ type ArrayMinBenchmark<'T when 'T : comparison>() =
     member this.Setup() =
         this.Array <- ArrayBenchmark.generateArray<'T> this.ArraySize
 
-    [<Benchmark(Description="Array.min - FSharp.Core")>]
-    member this.MinDefault() =
-            Microsoft.FSharp.Collections.Array.min this.Array
-
     [<Benchmark(Description="Array.min - FSharp.Core.Faster")>]
     member this.MinFaster() =
         FSharp.Core.Faster.Collections.Array.Array.min this.Array
+
+    [<Benchmark(Description="Array.min - FSharp.Core")>]
+    member this.MinDefault() =
+            Microsoft.FSharp.Collections.Array.min this.Array
