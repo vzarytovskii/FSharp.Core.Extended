7b22f373c2e554a8d8971b29f5730a543298f809
Vlad Zarytovskii <vzaritovsky@hotmail.com>
2024-06-19 18:06:59 +0200

    wip

--- src/PrimTypes.fs
+++ src/PrimTypes.fs
@@ -1,33 +1,24 @@
 namespace FSharp.Core.Extended
 
-#nowarn "1204" // Compiler-only usage warnings
-#nowarn "3391" // op_Implicit conversions warning
-#nowarn "9"
-
 [<AutoOpen>]
 module Locks =
-    [<AbstractClass; Sealed; NoComparison; NoEquality>]
-    type Lock =
-        static member inline Lock ((lockObject: System.Threading.Lock, action: unit -> 'a), _: Lock) : 'a =
-            let scope = lockObject.EnterScope()
-            try
-                action ()
-            finally
-                scope.Dispose()
 
-        static member inline Lock ((lockObject: obj, action: unit -> 'a), _: Lock) : 'a =
-            assert (lockObject.GetType() <> typeof<System.Threading.Lock>)
+    [<AutoOpen; AbstractClass; Sealed; NoComparison; NoEquality>]
+    type Lock() =
+        static member inline lock (lockObject: System.Threading.Lock) =
+            fun ([<InlineIfLambda>] action: unit -> 'R) ->
+                assert (lockObject.GetType() = typeof<System.Threading.Lock>)
+                let scope = lockObject.EnterScope()
+                try
+                    action ()
+                finally
+                    scope.Dispose()
 
-            let mutable lockTaken = false
-            try
-                System.Threading.Monitor.Enter(lockObject, &lockTaken)
-                action ()
-            finally
-                if lockTaken then
+        static member inline lock (lockObject: 'T when 'T : not struct) =
+            fun ([<InlineIfLambda>] action: unit -> 'R) ->
+                assert (lockObject.GetType() <> typeof<System.Threading.Lock>)
+                System.Threading.Monitor.Enter(lockObject)
+                try
+                    action ()
+                finally
                     System.Threading.Monitor.Exit(lockObject)
-
-        static member inline Invoke (action: unit -> 'U) (lockObject: 'L) : 'U =
-                let inline call (mthd: ^M, lockObject: ^O, _output: ^R) = ((^M or ^O or ^R) : (static member Lock : (_ * _) * _ -> _) (lockObject, action), mthd)
-                call (Unchecked.defaultof<Lock>, lockObject, Unchecked.defaultof<'U>)
-
-    let inline lock (lockObject: 'T when 'T : not struct) (action: unit -> 'a) : 'a = Lock.Invoke action lockObject
--- tests/Core/Lock.fs
+++ tests/Core/Lock.fs
@@ -4,25 +4,48 @@ module Min =
     open Expecto
     open FSharp.Core.Extended.Locks
 
-    let config = { FsCheckConfig.defaultConfig with maxTest = 100 }
 
-    let lockTest syncRoot (count: int) =
-        let mutable k = 0
-        let comp syncRoot _ =
-            async {
-                return lock syncRoot (
-                    fun () ->
-                        k <- k + 1
-                        System.Threading.Thread.Sleep(1)
-                        k
-                )
-            }
-        let arr = Async.RunSynchronously (Async.Parallel(Seq.map (comp syncRoot) [1..count]))
-        Expect.equal [|1..count|] (Array.sort arr) $"Expected {[1..count]} but was {arr}"
+    let private config =
+        { Expecto.FsCheckConfig.defaultConfig with
+            maxTest = 100 }
+
 
     [<Tests>]
     let tests =
         testList "Lock" [
-            testPropertyWithConfig config "System.Object lock works as expected" (lockTest (System.Object()))
-            testPropertyWithConfig config "System.Threading.Lock lock works as expected" (lockTest (System.Threading.Lock()))
+            // These two will throw in DEBUG if it goes to the wrong lock implementation (via assert)
+            testCase "System.Object is accepted in the lock function" <| fun () -> lock (obj()) (fun () -> ())
+            testCase "System.Threading.Lock is accepted in the lock function" <| fun () -> lock (System.Threading.Lock()) (fun () -> ())
+            testPropertyWithConfig config "System.Object lock works as expected" <| fun (maxvalue: int) ->
+                let syncRoot = System.Object()
+                let mutable k = 0
+                let comp _ =
+                    async {
+                        return lock syncRoot <| fun () ->
+                            k <- k + 1
+                            System.Threading.Thread.Sleep(1)
+                            k
+                    }
+                let arr = Async.RunSynchronously (Async.Parallel(Seq.map comp [1 .. maxvalue]))
+
+                let expected = [| 1 .. maxvalue |]
+                let actual = Array.sort arr
+
+                Expect.equal expected actual $"Expected the array to be {expected} but was {actual}"
+            testPropertyWithConfig config "System.Threading.Lock lock works as expected" <| fun (maxvalue: int) ->
+                let syncRoot = System.Threading.Lock()
+                let mutable k = 0
+                let comp _ =
+                    async {
+                        return lock syncRoot <| fun () ->
+                            k <- k + 1
+                            System.Threading.Thread.Sleep(1)
+                            k
+                    }
+                let arr = Async.RunSynchronously (Async.Parallel(Seq.map comp [1 .. maxvalue]))
+
+                let expected = [| 1 .. maxvalue |]
+                let actual = Array.sort arr
+
+                Expect.equal expected actual $"Expected the array to be {expected} but was {actual}"
         ]
