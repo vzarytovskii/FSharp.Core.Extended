b1ecceb3c230ccc7a6a3801a3dd3bace97d7135a
Vlad Zarytovskii <vzaritovsky@hotmail.com>
2024-06-19 18:46:13 +0200

    Docs

--- src/PrimTypes.fs
+++ src/PrimTypes.fs
@@ -5,6 +5,53 @@ module Locks =
 
     [<AutoOpen; AbstractClass; Sealed; NoComparison; NoEquality>]
     type Lock() =
+        /// <summary>Execute the function as a mutual-exclusion region using the input value as a lock. </summary>
+        ///
+        /// <param name="lockObject">The object to be locked.</param>
+        /// <param name="action">The action to perform during the lock.</param>
+        ///
+        /// <returns>The resulting value.</returns>
+        ///
+        /// <example id="lock-example">
+        /// <code lang="fsharp">
+        /// open System.Linq
+        ///
+        /// /// A counter object, supporting unlocked and locked increment
+        /// type TestCounter () =
+        ///     let mutable count = 0
+        ///
+        ///     /// Increment the counter, unlocked
+        ///     member this.IncrementWithoutLock() =
+        ///         count &lt;- count + 1
+        ///
+        ///     /// Increment the counter, locked
+        ///     member this.IncrementWithLock() =
+        ///         lock this (fun () -> count &lt;- count + 1)
+        ///
+        ///     /// Get the count
+        ///     member this.Count = count
+        ///
+        /// let counter = TestCounter()
+        ///
+        /// // Create a parallel sequence to that uses all our CPUs
+        /// (seq {1..100000}).AsParallel()
+        ///     .ForAll(fun _ -> counter.IncrementWithoutLock())
+        ///
+        /// // Evaluates to a number between 1-100000, non-deterministically because there is no locking
+        /// counter.Count
+        ///
+        /// let counter2 = TestCounter()
+        ///
+        /// //  Create a parallel sequence to that uses all our CPUs
+        /// (seq {1..100000}).AsParallel()
+        ///     .ForAll(fun _ -> counter2.IncrementWithLock())
+        ///
+        /// //  Evaluates to 100000 deterministically because the increment to the counter object is locked
+        /// counter2.Count
+        /// </code>
+        /// </example>
+        ///
+        [<CompiledName("Lock")>]
         static member inline lock (lockObject: System.Threading.Lock) =
             fun ([<InlineIfLambda>] action: unit -> 'R) ->
 #if DEBUG
@@ -17,6 +64,53 @@ module Locks =
                 finally
                     scope.Dispose()
 
+        /// <summary>Execute the function as a mutual-exclusion region using the input value as a lock. </summary>
+        ///
+        /// <param name="lockObject">The object to be locked.</param>
+        /// <param name="action">The action to perform during the lock.</param>
+        ///
+        /// <returns>The resulting value.</returns>
+        ///
+        /// <example id="lock-example">
+        /// <code lang="fsharp">
+        /// open System.Linq
+        ///
+        /// /// A counter object, supporting unlocked and locked increment
+        /// type TestCounter () =
+        ///     let mutable count = 0
+        ///
+        ///     /// Increment the counter, unlocked
+        ///     member this.IncrementWithoutLock() =
+        ///         count &lt;- count + 1
+        ///
+        ///     /// Increment the counter, locked
+        ///     member this.IncrementWithLock() =
+        ///         lock this (fun () -> count &lt;- count + 1)
+        ///
+        ///     /// Get the count
+        ///     member this.Count = count
+        ///
+        /// let counter = TestCounter()
+        ///
+        /// // Create a parallel sequence to that uses all our CPUs
+        /// (seq {1..100000}).AsParallel()
+        ///     .ForAll(fun _ -> counter.IncrementWithoutLock())
+        ///
+        /// // Evaluates to a number between 1-100000, non-deterministically because there is no locking
+        /// counter.Count
+        ///
+        /// let counter2 = TestCounter()
+        ///
+        /// //  Create a parallel sequence to that uses all our CPUs
+        /// (seq {1..100000}).AsParallel()
+        ///     .ForAll(fun _ -> counter2.IncrementWithLock())
+        ///
+        /// //  Evaluates to 100000 deterministically because the increment to the counter object is locked
+        /// counter2.Count
+        /// </code>
+        /// </example>
+        ///
+        [<CompiledName("Lock")>]
         static member inline lock (lockObject: 'T when 'T : not struct) =
             fun ([<InlineIfLambda>] action: unit -> 'R) ->
 #if DEBUG
